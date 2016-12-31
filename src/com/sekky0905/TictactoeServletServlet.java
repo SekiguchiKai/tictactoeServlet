9e24a2efe2844608472651dd08c26ba78c026dbf
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2016-12-31 22:33:20 +0900

    doGetを変更

--- src/com/sekky0905/TictactoeServletServlet.java
+++ src/com/sekky0905/TictactoeServletServlet.java
@@ -1,12 +1,19 @@
 package com.sekky0905;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class TictactoeServletServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html; charset=UTF8");
+
+		PrintWriter printWriter = resp.getWriter();
+
+		printWriter.println("Hello, world");
 	}
 }
